d880059dd4130330948248381590c6fd7a26212e
SejalKhadse <108009299+SejalKhadse@users.noreply.github.com>
2025-03-26 08:59:19 +0530

    Update backend.tf

--- eks/backend.tf
+++ eks/backend.tf
@@ -10,7 +10,7 @@ terraform {
     bucket         = "dev-sejal1-tf-bucket"
     region         = "us-east-1"
     key            = "eks/terraform.tfstate"
-    dynamodb_table = "Lock-Files"
+    use_lock_table = true
     encrypt        = true
   }
 }
